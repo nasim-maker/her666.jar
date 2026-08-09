e25291b52e6437a675ff39361c786fc7941e06db
nasim-maker <oumaramel9@gmail.com>
2026-08-09 15:46:45 +0100

    Rename ExampleMod to Her666 with updated mod details

--- src/main/java/studio/dreamys/ExampleMod.java
+++ src/main/java/studio/dreamys/ExampleMod.java
@@ -4,14 +4,16 @@ import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod {
-    public static final String MODID = "examplemod";
+@Mod(modid = Her666.MODID, name = Her666.NAME, version = Her666.VERSION)
+public class Her666 {
+
+    public static final String MODID = "her666";
+    public static final String NAME = "Her666";
     public static final String VERSION = "1.0";
-    
+
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
-		// some example code
+        System.out.println("Her666 has loaded!");
         System.out.println("DIRT BLOCK >> " + Blocks.dirt.getUnlocalizedName());
     }
 }
